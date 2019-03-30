d78c0b2e229a64fd8dc951ef141d052c6fea6818
Polako912 <michal.polak37@gmail.com>
2019-03-30 15:52:27 +0100

    Poruszanie graczem i umozliwienie niszczenia asteroidy

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32.SafeHandles;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [System.Serializable]
+    public class Boundary
+    {
+        public float xMin, zMin, xMax, zMax;
+    }
+
+    public class PlayerController : MonoBehaviour
+    {   
+        public float speed;
+        public Rigidbody rb;
+        public Boundary Boundary;
+        public float tilt;
+
+        public GameObject shot;
+        public Transform shotSpawn;
+        public float fireRate;
+
+        public float nextFire;
+
+        void Update()
+        {
+            if (Input.GetButton("Fire1") && Time.time > nextFire)
+            {
+                nextFire = fireRate + Time.time;
+                Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            }
+        }
+        void FixedUpdate()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+            rb.velocity = movement * speed;
+            rb.position = new Vector3
+                (
+                    Mathf.Clamp(rb.position.x, Boundary.xMin, Boundary.xMax), 
+                    0.0f, 
+                    Mathf.Clamp(rb.position.z, Boundary.zMin, Boundary.zMax)
+                );
+
+            rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+        }
+
+        
+    }
+}
