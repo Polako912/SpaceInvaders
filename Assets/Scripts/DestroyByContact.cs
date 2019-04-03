f9328e7d4fe462d8e25f2d3ad32d231ac92cfdf3
Polako912 <michal.polak37@gmail.com>
2019-04-03 16:16:34 +0200

    Dodanie efektow eksplozji, asteroidy pojawiają się w falach

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyByContact : MonoBehaviour {
+public class DestroyByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag != "Boundary")
         {
+            Instantiate(explosion, transform.position, transform.rotation);
+            
             Destroy(other.gameObject);
             Destroy(gameObject);
+        }
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
         }
     }
 }
