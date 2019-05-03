4a0b75cffb445f0ed13c71aeb14d5f28d972949b
Polako912 <michal.polak37@gmail.com>
2019-05-03 16:21:27 +0200

    podpiecie innych przyciskow, mozliwosc poruszania sie po menu za pomoca klawiatury

---
+++ Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour
+{
+    public EventSystem eventSystem;
+    public GameObject SelectedGameObject;
+
+    private bool selected = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Vertical") != 0 && selected == false)
+        {
+            eventSystem.SetSelectedGameObject(SelectedGameObject);
+            selected = true;
+        }
+    }
+
+    private void Disable()
+    {
+        selected = false;
+    }
+}
